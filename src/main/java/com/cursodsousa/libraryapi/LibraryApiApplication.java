87a98dfbeb21e1f4e12c696f08e5204975f8030f
Dougllas Sousa <dougllasfps@gmail.com>
2019-12-05 16:47:33 -0300

    configurando o codecov badge

--- src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
@@ -1,11 +1,13 @@
 package com.cursodsousa.libraryapi;
 
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+//@EnableAdminServer
 public class LibraryApiApplication {
 
 	@Bean
