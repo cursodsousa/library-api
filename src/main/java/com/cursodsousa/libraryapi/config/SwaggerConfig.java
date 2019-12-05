ac34046ec1dfe43f76cb5518995e99bae4c08799
Dougllas Sousa <dougllasfps@gmail.com>
2019-12-04 22:41:37 -0300

    adicionando o swagger

--- src/main/java/com/cursodsousa/libraryapi/config/SwaggerConfig.java
+++ src/main/java/com/cursodsousa/libraryapi/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,6 +30,7 @@ public class SwaggerConfig {
                     .title("Library API")
                     .description("Documentação da API de bibliotecas.")
                     .version("1.0")
+                    .contact(new Contact("Dougllas Sousa", "http://github.com/dougllasfps", "dougllasfps@gmail.com"))
                     .build();
     }
 }
