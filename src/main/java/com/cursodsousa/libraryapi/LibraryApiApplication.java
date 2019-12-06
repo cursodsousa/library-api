d21e80db29c71490c20aec241af7d5b6a5f62664
Dougllas Sousa <dougllasfps@gmail.com>
2019-12-06 10:54:40 -0300

    configuração de email

--- src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
@@ -1,18 +1,36 @@
 package com.cursodsousa.libraryapi;
 
-//import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
-//@EnableAdminServer
 public class LibraryApiApplication {
+
+	@Autowired
+	private JavaMailSender mailSender;
 
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+//	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+			SimpleMailMessage mailMessage = new SimpleMailMessage();
+			mailMessage.setFrom("library@mail.com");
+			mailMessage.setSubject("Test");
+			mailMessage.setText("text");
+			mailMessage.setTo("dougllasfps@gmail.com");
+			mailSender.send(mailMessage);
+		};
 	}
 
 	public static void main(String[] args) {
